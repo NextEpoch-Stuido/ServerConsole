58f1ced51d85f819016631b5dd87e85063197f17
to0c <toyyds2022@163.com>
2026-08-27 23:08:08 +0800

    111

--- ServerConsole/Program.cs
+++ ServerConsole/Program.cs
@@ -7,7 +7,7 @@ public class Program
 {
     public static readonly string EXE_PATH = "SiteFrostfall.exe";
     private const int DEFAULT_PORT = 7777;
-    // 设为静态方便指令集调用（如 ExitCommand）
+    private static readonly Random random = new Random();
     public static ServerProcess? ServerInstance { get; private set; }
 
     public static void Main(string[] args)
@@ -25,7 +25,7 @@ public class Program
             Console.ResetColor();
             string? input = Console.ReadLine();
             if (int.TryParse(input, out int p) && IsValidPort(p)) port = p;
-            else Logger.Print("Invalid port number. Please enter a number between 1 and 65535.",ConsoleColor.Red);
+            else Logger.Print("Invalid port number. Please enter a number between 1 and 65535.", ConsoleColor.Red);
         }
 
         Logger.InternalLog_h($"Initializing site on PORT: {port}", LogLevel.Info);
@@ -60,17 +60,28 @@ public class Program
             Console.Title = "SiteFrostfall | Dedicated Server Console";
         }
     }
-
-    private static void PrintBanner()
-    {
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine(@"
+    const string Banner_1 = @"
  ███████╗██╗████████╗███████╗          ███████╗██████╗  ██████╗ ███████╗████████╗███████╗ █████╗ ██╗     ██╗     
  ██╔════╝██║╚══██╔══╝██╔════╝          ██╔════╝██╔══██╗██╔═══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗██║     ██║     
  ███████╗██║   ██║   █████╗   ███████╗ █████╗  ██████╔╝██║   ██║███████╗   ██║   █████╗  ███████║██║     ██║     
  ╚════██║██║   ██║   ██╔══╝   ╚══════╝ ██╔══╝  ██╔══██╗██║   ██║╚════██║   ██║   ██╔══╝  ██╔══██║██║     ██║     
  ███████║██║   ██║   ███████╗          ██║     ██║  ██║╚██████╔╝███████║   ██║   ██║     ██║  ██║███████╗███████╗
- ╚══════╝╚═╝   ╚═╝   ╚══════╝          ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝");
+ ╚══════╝╚═╝   ╚═╝   ╚══════╝          ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝";
+    const string Banner_2 = @"
+ ███████╗██╗████████╗███████╗          ██╗ ██╗ ██╗██╗ ███║    ██╗████████╗███████╗██████╗
+ ██╔════╝██║╚══██╔══╝██╔════╝          ██║ ██║ ██║██║ ██║██║  ██║╚══██╔══╝██╔════╝██╔══██╗
+ ███████╗██║   ██║   █████╗   ███████╗ ██║ ██║ ██║██║ ██║ ██║ ██║   ██║   █████╗  ██████╔╝
+ ╚════██║██║   ██║   ██╔══╝   ╚══════╝ ██║ ██║ ██║██║ ██║  ██║██║   ██║   ██╔══╝  ██╔══██╗
+ ███████║██║   ██║   ███████╗          ╚████████╝ ██║ ██║   ████║   ██║   ███████╗██║  ██║
+ ╚══════╝╚═╝   ╚═╝   ╚══════╝           ╚══════╝  ╚═╝ ╚═╝   ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝";
+    private static string GetBanner()
+    {
+        return (random.Next(100)<30) ? Banner_2 : Banner_1;
+    }
+    private static void PrintBanner()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine(GetBanner());
 
         Console.ForegroundColor = ConsoleColor.DarkGray;
         Console.WriteLine(" -----------------------------------------------------------------------------------------------------------------");
